58c5278d5426e27b51b8ff9334a96e34a99603dc
JATIN KUMAR <jatinkumar638881@gmail.com>
2026-09-02 00:27:22 +0530

    Update production API URL

--- frontend/src/api/apiEndpoints.js
+++ frontend/src/api/apiEndpoints.js
@@ -1,6 +1,6 @@
 const API_BASE_URL =
   import.meta.env.VITE_API_BASE_URL ||
-  "http://localhost:8081/api/v1.0";
+  "http://instant-mechanic-api.jatindev.xyz/api/v1.0";
 
 const API_ENDPOINTS = {
   BASE_URL: API_BASE_URL,
